aca46cdb843163a47d3fe01d2ff91a6ef9e5349c
JeanPierre <arenaspierre@protonmail.com>
2018-05-21 18:40:27 -0500

    mejorando mi invie con la agregación de videos y lazy load

--- script/script.js
+++ script/script.js
@@ -1,6 +1,21 @@
 let $burguerButton = document.getElementById('burguer-button');
 let $menu = document.getElementById('menu');
+let consulta = window.matchMedia("(max-width: 500px)")
+consulta.addListener(mediaQuery);
 
 const toggleMenu = () => $menu.classList.toggle('active')
 
-$burguerButton.addEventListener('touchstart', toggleMenu);
+function mediaQuery() {
+  if (consulta.matches) {
+    console.log("se cumplio la condicción");
+    $burguerButton.addEventListener('touchstart', toggleMenu);
+
+  } else {
+    console.log("no se cumplio la condicción");
+    $burguerButton.removeEventListener('touchstart', toggleMenu);
+  }
+}
+
+var bLazy = new Blazy({
+  selector:'img'
+});
